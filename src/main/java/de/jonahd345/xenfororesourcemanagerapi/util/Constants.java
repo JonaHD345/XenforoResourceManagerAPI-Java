6310691413b6025df62fcdfd858c064540d02ee3
jonahd345 <contact@jonahd345.de>
2025-02-23 11:55:04 +0100

    feat: add Constants class for holding API-related constant values

---
+++ src/main/java/de/jonahd345/xenfororesourcemanagerapi/util/Constants.java
@@ -0,0 +1,11 @@
+package de.jonahd345.xenfororesourcemanagerapi.util;
+
+/**
+ * A utility class that holds constant values used throughout the Xenforo Resource Manager API.
+ */
+public class Constants {
+    /**
+     * The base URL for the API.
+     */
+    public static final String API_URL = "https://api.spigotmc.org/simple/0.2/index.php";
+}
