d34bd028fca7b83681dacaafad8d5d15a3ffd164
jonahd345 <contact@jonahd345.de>
2025-02-23 17:20:18 +0100

    feat: enhance Author model with resource count, identities, and avatar URL

--- src/main/java/de/jonahd345/xenfororesourcemanagerapi/model/Author.java
+++ src/main/java/de/jonahd345/xenfororesourcemanagerapi/model/Author.java
@@ -1,10 +1,11 @@
 package de.jonahd345.xenfororesourcemanagerapi.model;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
 /**
  * Represents an author in the Xenforo Resource Manager API.
- * This class contains the author's ID and username.
+ * This class contains the author's ID, username, resource count, identities, and avatar URL.
  */
 @Data
 public class Author {
@@ -17,4 +18,20 @@ public class Author {
      * The username of the author.
      */
     private String username;
+
+    /**
+     * The number of resources created by the author.
+     */
+    @SerializedName("resource_count")
+    private int resourceCount;
+
+    /**
+     * The social media and communication platform identifiers of the author.
+     */
+    private Identities identities;
+
+    /**
+     * The URL of the author's avatar image.
+     */
+    private String avatar;
 }
